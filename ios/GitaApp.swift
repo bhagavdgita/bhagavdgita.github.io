2bc06d5095e5c2bcf0f4805046898c5cdfde0d79
bhagavdgita <111245279+bhagavdgita@users.noreply.github.com>
2022-08-18 12:44:08 -0400

    Create GitaApp.swift

---
+++ ios/GitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitaApp.swift
+//  Gita
+//
+//  Created by SHASHI JAKKIPALLY on 4/13/22.
+//
+
+import SwiftUI
+
+@main
+struct GitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
