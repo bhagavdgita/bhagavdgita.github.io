d7de599a164b729557287b0458be61d72f9ec4e9
bhagavdgita <111245279+bhagavdgita@users.noreply.github.com>
2022-08-25 13:57:37 -0400

    Create main.py

---
+++ storage/api/verseoftheday/main.py
@@ -0,0 +1,41 @@
+from flask import Flask,jsonify
+import random, datetime
+
+app = Flask(__name__)
+
+
+def getshlokaforday():
+    x = datetime.datetime.today().strftime("%Y:%m:%d")
+    random.seed(x)
+    shlokas = [
+        "0",
+        "47",
+        "72",
+        "43",
+        "42",
+        "29",
+        "47",
+        "30",
+        "28",
+        "34",
+        "42",
+        "55",
+        "20",
+        "35",
+        "27",
+        "20",
+        "24",
+        "28",
+        "78",
+    ]
+    r_int = random.randint(1, 18)
+    r1_int = random.randint(1, int(shlokas[r_int]))
+    return [r_int, r1_int]
+
+
+@app.route("/")
+def index():
+    return jsonify([getshlokaforday()[0], getshlokaforday()[1]])
+
+
+app.run(host="0.0.0.0", port=81)
