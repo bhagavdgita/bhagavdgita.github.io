15a0f05e0b9f477d03bea93e9c7c69d0291f1f51
Arjun Jakkipally <68869672+arjunj132@users.noreply.github.com>
2023-08-03 10:32:51 -0400

    Update GitaApp.swift

--- ios/GitaApp.swift
+++ ios/GitaApp.swift
@@ -6,12 +6,35 @@
 //
 
 import SwiftUI
+import Network
+
+// code from https://www.danijelavrzan.com/posts/2022/11/network-connection-alert-swiftui/
+class NetworkMonitor: ObservableObject {
+    private let networkMonitor = NWPathMonitor()
+    private let workerQueue = DispatchQueue(label: "Monitor")
+    var isConnected = false
+
+    init() {
+        networkMonitor.pathUpdateHandler = { path in
+            self.isConnected = path.status == .satisfied
+            Task {
+                await MainActor.run {
+                    self.objectWillChange.send()
+                }
+            }
+        }
+        networkMonitor.start(queue: workerQueue)
+    }
+}
+
 
 @main
 struct GitaApp: App {
+    @StateObject var networkMonitor = NetworkMonitor()
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(networkMonitor)
         }
     }
 }
